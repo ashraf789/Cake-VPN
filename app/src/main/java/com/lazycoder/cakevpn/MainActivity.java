8984fb17236fa172e9ce8834366a488947885d4a
ashraf789 <syedashrafullah15@gmail.com>
2020-01-01 13:35:08 +0700

    right navigationView add

--- app/src/main/java/com/lazycoder/cakevpn/MainActivity.java
+++ app/src/main/java/com/lazycoder/cakevpn/MainActivity.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
-import androidx.databinding.DataBindingUtil;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
@@ -13,20 +12,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 
-import com.lazycoder.cakevpn.databinding.ActivityMainBinding;
 
-/**
- * This activity will handle all VPN on/off , server change etc request
- */
 public class MainActivity extends AppCompatActivity {
     private FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
     private Fragment fragment;
-
-    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        setContentView(R.layout.activity_main);
         ImageButton menuRight = findViewById(R.id.navbar_right);
 
 
@@ -37,8 +30,6 @@ public class MainActivity extends AppCompatActivity {
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
-
-        toggle.syncState();
 
         menuRight.setOnClickListener(new View.OnClickListener() {
             @Override
